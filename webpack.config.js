43db326ba126ab8aa0ce46b51ce659b94bab40ab
Tim Warrington <tim.warrin@gmail.com>
2019-01-27 00:03:09 -0700

    updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
@@ -9,11 +10,13 @@ module.exports = (env, argv) => {
 
   return {
     mode: argv.mode,
+    devtool: devMode ? 'cheap-eval-source-map' : 'source-map',
     entry: {
       main: ['./assets/javascripts/main.js', './assets/stylesheets/main.css'],
     },
     output: {
-      filename: devMode ? '[name].js' : '[name].[chunkhash:8].js',
+      filename: devMode ? 'js/[name].js' : 'js/[name].[chunkhash:8].js',
+      publicPath: '/',
       path: path.resolve(__dirname, 'static/assets'),
     },
     module: {
@@ -28,26 +31,46 @@ module.exports = (env, argv) => {
           include: path.resolve(__dirname, 'assets'),
           use: [
             { loader: MiniCssExtractPlugin.loader },
-            'css-loader',
-            'postcss-loader',
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true },
+            },
           ],
         },
       ],
     },
     plugins: [
+      new FixStyleOnlyEntriesPlugin(),
       new MiniCssExtractPlugin({
-        filename: devMode ? '[name].css' : '[name].[contenthash:8].css',
-        chunkFilename: devMode ? '[id].css' : '[id].[contenthash:8].css',
+        filename: devMode ? 'css/[name].css' : 'css/[name].[contenthash:8].css',
+        chunkFilename: devMode
+          ? 'css/[id].css'
+          : 'css/[id].[contenthash:8].css',
       }),
-      devMode
-        ? null
-        : new ManifestPlugin({
-            publicPath: 'assets/',
-            fileName: path.resolve(__dirname, 'data/manifest.json'),
-          }),
-    ].filter(Boolean),
+      ManifestPlugin({
+        publicPath: 'assets/',
+        fileName: path.resolve(__dirname, 'data/manifest.json'),
+      }),
+    ],
     optimization: {
-      minimizer: [new OptimizeCSSAssetsPlugin({}), new UglifyWebpackPlugin({})],
+      splitChunks: {
+        chunks: 'all',
+      },
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            map: {
+              inline: false,
+              annotation: true,
+            },
+          },
+        }),
+        new UglifyWebpackPlugin({ sourceMap: true }),
+      ],
     },
   };
 };
