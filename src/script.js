7e08064ed060e5cae3a42b05a1bfba83e1b3ea77
Timothy Warrington <tim.warrin@gmail.com>
2020-07-14 21:31:37 -0600

    Update packages, add gitlab link

--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
 (() => {
-  let eyeLeft = document.querySelector('.left');
-  let eyeRight = document.querySelector('.right');
+  let eyeLeft = document.querySelector(".left");
+  let eyeRight = document.querySelector(".right");
 
-  document.addEventListener('mousemove', event => {
+  document.addEventListener("mousemove", (event) => {
     let rectLeft = eyeLeft.getBoundingClientRect();
-    let xLeft = (rectLeft.left) + (eyeLeft.offsetWidth / 2.5);
-    let yLeft = (rectLeft.top) + (eyeLeft.offsetHeight / 2.5);
+    let xLeft = rectLeft.left + eyeLeft.offsetWidth / 2.5;
+    let yLeft = rectLeft.top + eyeLeft.offsetHeight / 2.5;
     let radLeft = Math.atan2(event.pageX - xLeft, event.pageY - yLeft);
-    let rotLeft = (radLeft * (180 / Math.PI) * -1) + 180;
-    eyeLeft.setAttribute('style', `transform: rotate(${rotLeft}deg);`);
+    let rotLeft = radLeft * (180 / Math.PI) * -1 + 180;
+    eyeLeft.setAttribute("style", `transform: rotate(${rotLeft}deg);`);
 
     let rectRight = eyeRight.getBoundingClientRect();
-    let xRight = (rectRight.left) + (eyeRight.offsetWidth / 1.5);
-    let yRight = (rectRight.top) + (eyeRight.offsetHeight / 1.5);
+    let xRight = rectRight.left + eyeRight.offsetWidth / 1.5;
+    let yRight = rectRight.top + eyeRight.offsetHeight / 1.5;
     let radRight = Math.atan2(event.pageX - xRight, event.pageY - yRight);
-    let rotRight = (radRight * (180 / Math.PI) * -1) + 180;
-    eyeRight.setAttribute('style', `transform: rotate(${rotRight}deg);`);
+    let rotRight = radRight * (180 / Math.PI) * -1 + 180;
+    eyeRight.setAttribute("style", `transform: rotate(${rotRight}deg);`);
   });
 })();
